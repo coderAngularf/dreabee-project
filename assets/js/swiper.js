c1269fbd8839469a0f8a25a85aa69de5f6429114
RizviSadiya <sadiyarizvi123@gmail.com>
2025-02-16 00:14:03 +0530

    menuset all pages

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -37,19 +37,34 @@
     });
 
     // dynamic pagination
-    var swiper = new Swiper(".pagination-dynamic", {
-        pagination: {
-            el: ".swiper-pagination",
-            dynamicBullets: true,
-            clickable: true,
-        },
-        loop: true, 
+// for testimonials
+var swiper = new Swiper(".pagination-dynamic", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      },
+      1400: {
         slidesPerView: 3,
-        autoplay: {
-            delay: 1500,
-            disableOnInteraction: false
-        }
-    });
+        spaceBetween: 50,
+      },
+    },
+  });
 
     // pagination with progress
     var swiper = new Swiper(".pagination-progress", {
